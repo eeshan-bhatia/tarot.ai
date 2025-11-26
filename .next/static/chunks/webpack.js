7ef736631dcec41c84c660cca5d1cb4ffea2001e
eeshan-bhatia <eeshan.bhatia.2003@gmail.com>
2025-11-26 20:04:01 +1300

    Removed .env and ds store as well as other gitignores

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43773a5ed5e8c33e"; }
+/******/ 		__webpack_require__.h = function() { return "feeb7e3f31f35b76"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
